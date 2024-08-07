5d30ae65fc80ee30fcb533c1eb546b154ae5fc68
Andrés Virga <andvirga@gmail.com>
2024-08-07 19:26:07 -0300

    chore: finished pure HTML table

---
+++ src/app/components/SortHeader.tsx
@@ -0,0 +1,32 @@
+import { MouseEvent } from "react";
+import { SortBy, TableRow } from "../types";
+import { SortIcon } from "./SortIcon";
+
+export interface ISortHeaderProps {
+  sortBy?: SortBy;
+  label: string;
+  column: keyof TableRow | "bounce" | "time";
+  onChangeHandler: any;
+}
+
+export const SortHeader = ({
+  sortBy,
+  label,
+  column,
+  onChangeHandler,
+}: ISortHeaderProps): JSX.Element | null => {
+  const processClick = (e: MouseEvent<HTMLAnchorElement, any>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onChangeHandler(column);
+  };
+
+  return (
+    <div className="sort-header">
+      <a href="/" onClick={(e) => processClick(e)}>
+        {label}
+      </a>
+      {sortBy?.key === column && <SortIcon direction={sortBy.direction} />}
+    </div>
+  );
+};
